25184948380420674a800e184c4077b3a53c9e0d
Michael Wheeler <mike214@live.co.uk>
2018-10-03 11:15:30 +0100

    Added Controller Added strava Token class moved strava auth logic to controller started building logic to parse access token response for athelete info and accesstoken

--- src/main/java/com/cycle_saver/converter/JSONConverter.java
+++ src/main/java/com/cycle_saver/converter/JSONConverter.java
@@ -1,4 +1,33 @@
-package com.cycle_saver.converter;
-
-public class JSONConverter {
-}
+//package com.cycle_saver.converter;
+//
+//import com.cycle_saver.controller.StravaAuthController;
+//import com.cycle_saver.model.Athlete;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//import java.io.IOException;
+//
+//public class JSONConverter {
+//    public static void main(StravaAuthController stravaAuthController) throws IOException {
+//
+//        //read json file data to String
+//        byte[] jsonData = stravaAuthController
+//
+//        //create ObjectMapper instance
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        //convert json string to object
+//        Athlete athlete = objectMapper.readValue(jsonData, Athlete.class);
+//
+//        System.out.println("Athlete Object\n"+emp);
+//
+//        //convert Object to json string
+//        Athlete athlete1 = createAthlete();
+//        //configure Object mapper for pretty print
+//        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+//
+//        //writing to console, can write to any output stream such as file
+//        StringWriter stringAthlete = new StringWriter();
+//        objectMapper.writeValue(stringEmp, athlete1);
+//        System.out.println("Athlete JSON is\n"+stringAthlete);
+//    }
+//}
--- src/main/java/com/cycle_saver/model/StravaToken.java
+++ src/main/java/com/cycle_saver/model/StravaToken.java
@@ -1,4 +1,48 @@
 package com.cycle_saver.model;
 
 public class StravaToken {
+    private String token_type;
+
+    private String access_token;
+
+    private String athlete;
+
+    public StravaToken(String token_type, String access_token, String athlete) {
+        this.token_type = token_type;
+        this.access_token = access_token;
+        this.athlete = athlete;
+    }
+
+    public String getToken_type() {
+        return token_type;
+    }
+
+    public void setToken_type(String token_type) {
+        this.token_type = token_type;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getAthlete() {
+        return athlete;
+    }
+
+    public void setAthlete(String athlete) {
+        this.athlete = athlete;
+    }
+
+    @Override
+    public String toString() {
+        return "StravaToken{" +
+                "token_type='" + token_type + '\'' +
+                ", access_token='" + access_token + '\'' +
+                ", athlete='" + athlete + '\'' +
+                '}';
+    }
 }
